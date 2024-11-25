bf5fe8916cd10b17140f3baa6c3ce391714aae2a
andre panizza <andredeveloper45@gmail.com>
2024-11-25 15:11:19 -0300

    update code

--- Introducao_ao_desenv_Web/Projeto_Estudo/Calc_IMC/imc.js
+++ Introducao_ao_desenv_Web/Projeto_Estudo/Calc_IMC/imc.js
@@ -1,1 +1,10 @@
 // Javascript do Index.html projeto IMC - Learning Javascript Mini Projects
+
+/** vamos pegar os Ids dos campos que vou interagir */
+const nome = document.getElementById("nome");
+const altura = document.getElementById("altura");
+const peso = document.getElementById("peso");
+const calcular = document.getElementById("calcular");
+const resultado = document.getElementById("resultado");
+
+// o ponto serve para acessarmos uma propriedade dele;
