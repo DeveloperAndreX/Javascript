25d3c576fed7c728786375858ff8722e7f2a3488
andre panizza <andredeveloper45@gmail.com>
2024-11-25 14:32:30 -0300

    update

--- Introducao_ao_desenv_Web/exemplo-10.js
+++ Introducao_ao_desenv_Web/exemplo-10.js
@@ -1,3 +1,21 @@
 /**Aprendendo sobre children e insertBefore: */
+// recuperando as listas;
+
+var lista = document.getElementsByTagName("ol");
+//var itens = document.getElementsByTagName("li");
+//A variavel itens vai receber os elementos filhos desta Lista usando:
+var itens = lista.children;
+
+// vamos criar um novo itens que queremos inserir na lista:
+
+var novoItem = document.createElement("li");
+novoItem.textContent = "suco de laranja";
+
+//Para trabalhar com InsertBefore, selecionamos o elemento PAI
+// Passamos 02 parametros no InsertBefore("O novo item, e o novo item que vai entrar antes na lista");
+
+lista.insertBefore(novoItem, itens[0]); // vai inserir o suco de laranja antes do cafe;
 
 
+console.log(lista);
+// vamos especificar o indice da lista que queremos:
