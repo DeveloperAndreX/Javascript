a3889ebd7a85ad1732f29db57198ee48945a3031
andre panizza <andredeveloper45@gmail.com>
2024-11-29 21:43:27 -0300

    update code

--- Introducao_ao_desenv_Web/Projeto_Estudo/Lampada_/lampada.js
+++ Introducao_ao_desenv_Web/Projeto_Estudo/Lampada_/lampada.js
@@ -9,17 +9,17 @@ const lamp = document.getElementById('lamp');
 
 function lampOn(){
 // fazer a troca da imagem da lampada:
-    lamp.src = './img/ligada.jpg';
+    lamp.src = './img/ligada.jpeg';
 }
 function lampOff(){
-    lamp.src = './img/deslig.jpg';
+    lamp.src = './img/deslig.jpeg';
 }
 function lampBroken(){
-    lamp.src = './img/quebrada.jpg';
+    lamp.src = './img/quebrada.jpeg';
 }
 // adicionando um evento ao botão turnOn
 turnOn.addEventListener('click', lampOn );
 turnOff.addEventListener('click',lampOff);
 lamp.addEventListener('mouseover', lampOn);
 lamp.addEventListener('mouseleave', lampOff);
-lampOff.addEventListener('dblclick', lampBroken);
+lamp.addEventListener('dblclick', lampBroken);
