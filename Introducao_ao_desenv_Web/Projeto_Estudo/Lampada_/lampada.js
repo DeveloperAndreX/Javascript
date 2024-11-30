e31db8a220c936e942efe746b9e9a1cbf3bf68b8
andre panizza <andredeveloper45@gmail.com>
2024-11-29 21:30:58 -0300

    update code

---
+++ Introducao_ao_desenv_Web/Projeto_Estudo/Lampada_/lampada.js
@@ -0,0 +1,8 @@
+// iniciando o Javascript:
+// fazendo funcionar os botões chamado as variaveis de turnOn e turnOFf
+
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
+
+// adicionando um evento ao botão turnOn
