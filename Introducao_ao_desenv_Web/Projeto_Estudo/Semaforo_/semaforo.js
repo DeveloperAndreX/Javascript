db917efa70a47a64c80774f5beb062896274ee55
andre panizza <andredeveloper45@gmail.com>
2024-12-02 18:02:23 -0300

    update this code

--- Introducao_ao_desenv_Web/Projeto_Estudo/Semaforo_/semaforo.js
+++ Introducao_ao_desenv_Web/Projeto_Estudo/Semaforo_/semaforo.js
@@ -1,1 +1,18 @@
 // codigo javascript para o Semaforo funcionar;
+const img = document.getElementById('img');
+const buttons = document.getElementById('buttons');
+console.log(alert ("teste de Javascript!"));
+
+
+
+// criando uma arrow function da função trafficLight:
+// arrow function se declara assim () => {}
+/* eu vou receber um evento do addEventListener,
+então e importante verificar o que eu estou recebendo ! */
+
+const trafficLight = ( event) => {
+    console.log( event );
+    
+}
+buttons.addEventListener('click', trafficLight);
+
