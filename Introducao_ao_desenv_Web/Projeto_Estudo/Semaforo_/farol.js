cbc56a5bcfe757b46db118503289829c4aa263e9
andre panizza <andredeveloper45@gmail.com>
2024-12-02 18:26:44 -0300

    update this code

--- Introducao_ao_desenv_Web/Projeto_Estudo/Semaforo_/farol.js
+++ Introducao_ao_desenv_Web/Projeto_Estudo/Semaforo_/farol.js
@@ -7,12 +7,13 @@ console.log(alert ("teste de Javascript!"));
 // criando uma arrow function da função trafficLight:
 // arrow function se declara assim () => {}
 /* eu vou receber um evento do addEventListener,
-então e importante verificar o que eu estou recebendo ! */
+então e importante verificar o que eu estou recebendo ! 
+Desta forma ele traz o id do target no evento, note que o botão traz a cor do ID.
+*/
 
 const trafficLight = ( event) => {
-    console.log( event );
+    console.log( event.target.id );
     
-
 }
 
 buttons.addEventListener('click', trafficLight);
